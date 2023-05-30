4c8ddb5e857aca371309135b656741a4d67b3288
goldSunshine <892697603@qq.com>
2023-05-30 14:21:38 +0800

    启动文件

--- app/db/run_migrate.py
+++ app/db/run_migrate.py
@@ -1,13 +1,30 @@
+import sys
 from peewee_migrate import Router
 
 from app.db.base_model import db
 from app.demo import models as demo_models
 
-# 需要迁移的数据库表
+# 需要迁移的数据库表, 所有数据新增、修改的都需要引入
 migrate_tables = [demo_models]
 
-db.connect()
-router = Router(db, ignore="basemodel")
-# router.create(auto=migrate_tables)
-router.run()
-db.close()
+
+def create_migrate():
+    db.connect()
+    router = Router(db, ignore="basemodel")
+    router.create(auto=migrate_tables)
+    db.close()
+
+
+def run_migrate():
+    db.connect()
+    router = Router(db, ignore="basemodel")
+    router.run()
+    db.close()
+
+
+if __name__ == "__main__":
+    action = sys.argv[1]
+    if action == "run_migrate":
+        run_migrate()
+    elif action == "create_migrate":
+        create_migrate()
--- app/start.sh
+++ app/start.sh
@@ -1,2 +1,9 @@
-export PYTHONPATH=$(dirname $(pwd))
+
+# 表修改
+python db/run_migrate.py run_migrate
+
+# python 环境变量
+export PYTHONPATH = $PYTHONPATH:$(dirname $(pwd))
+
+# 启动flask
 python main.py
